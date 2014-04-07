d8934430ffabc7c400028c88024960ccacf55b3d
damiengo <dam.gou@gmail.com>
2014-04-07 19:03:39 +0200

    Deleted django files

--- photos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Photo(models.Model):
-    file_name   = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at  = models.DateTimeField()
-    updated_at  = models.DateTimeField()
