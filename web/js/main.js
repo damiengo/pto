0faeed41acda7de78bd9d07b7e028de2e21d7674
unknown <GOUGEON-55061@S154901-12-2750.d15490102.cti-paysloire.cnamts.fr>
2014-04-11 17:15:20 +0200

    New gallery field

--- web/js/main.js
+++ web/js/main.js
@@ -6,10 +6,24 @@ var ptoApp = angular.module("ptoApp", []);
 // Gallery controller
 ptoApp.controller("galleryCtrl", function($scope) {
 
+  /** Init datas **/
   $scope.galleries = [
     {'title': 'Gallery 1'},
     {'title': 'Gallery 2'},
     {'title': 'Gallery 3'}
   ];
+
+  /**
+   * Adding a new gallery.
+   *
+   */
+  $scope.add = function() {
+  	if($scope.galleryTitle != "") {
+      $scope.galleries.push({
+      	title: $scope.galleryTitle
+      });
+      $scope.galleryTitle = "";
+    }
+  }
 
 });
