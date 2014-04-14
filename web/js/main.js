851a1cb8595c7c3f8f560055546f4527cb4d8c6a
damiengo <dam.gou@gmail.com>
2014-04-14 16:13:56 +0200

    Image downloading

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 // Declaring the app
-var ptoApp = angular.module("ptoApp", []);
+var ptoApp = angular.module("ptoApp", ['flow']);
+
+// Configuration
+ptoApp.config(['flowFactoryProvider', function (flowFactoryProvider) {
+  flowFactoryProvider.defaults = {
+    target: 'http://localhost:8888/admin/upload',
+    permanentErrors: [404, 500, 501],
+    maxChunkRetries: 1,
+    chunkRetryInterval: 5000,
+    simultaneousUploads: 4
+  };
+  flowFactoryProvider.on('catchAll', function (event) {
+    console.log('catchAll', arguments);
+  });
+  // Can be used with different implementations of Flow.js
+  // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
 
 // Gallery controller
 ptoApp.controller("galleryCtrl", function($scope) {
