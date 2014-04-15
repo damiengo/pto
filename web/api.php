a37f86453143135ef8dcb4ccf66652e3451959db
damiengo <dam.gou@gmail.com>
2014-04-15 16:16:40 +0000

    Tests for files

--- web/api.php
+++ web/api.php
@@ -13,15 +13,53 @@ $app->register(new CorsServiceProvider(), array(
 ));
 
 // Routes
-$app->get('/admin/upload', function() use ($app) {
-    $request = $app['request'];
+$app->match('/admin/upload', function() use ($app) {
+    //if (\Flow\Basic::save('/tmp/final_file_destination', '/tmp/chunks_temp_folder')) {
+    //    echo "\nSaved!";
+    //}
+    //else {
+        // This is not a final chunk or request is invalid, continue to upload.
+    //    echo "\nNot final or invalid..";
+    //}
 
-    if (\Flow\Basic::save('/tmp/final_file_destination', '/tmp/chunks_temp_folder')) {
-        echo "\nSaved!";
+
+
+    $config = new \Flow\Config();
+    $config->setTempDir('/tmp/chunks_temp_folder');
+    $file = new \Flow\File($config);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        print_r($_FILES);
+        if ($file->checkChunk()) {
+            header("HTTP/1.1 200 Ok");
+            echo "check ok";
+        }
+        else {
+            header("HTTP/1.1 404 Not Found");
+            print_r($file);
+            echo "check ko";
+            return 0;
+        }
     }
     else {
-        // This is not a final chunk or request is invalid, continue to upload.
-        echo "\nNot final or invalid..";
+        if ($file->validateChunk()) {
+            echo "chunk validated";
+            $file->saveChunk();
+        }
+        else {
+            // error, invalid chunk upload request, retry
+            header("HTTP/1.1 400 Bad Request");
+            echo "chunk unvalidated";
+            return 0;
+        }
+    }
+    if ($file->validateFile() && $file->save('/tmp/final_file_name')) {
+        // File upload was completed
+        echo "upload completed";
+    }
+    else {
+        // This is not a final chunk, continue to upload
+        echo "not final chunk";
     }
 
     return 1;
